b9a4d7f076622e2e7f72cc53ffab93ccd02bf585
Nazim Huseynov <333nazim333@gmail.com>
2022-08-16 21:18:07 +0400

    added k8s-master.sh and k8s-worker.sh

---
+++ k8s-master.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+
+echo "##### Update the OS #####"
+yum update -y
+
+echo "##### Install yum-utils, bash completion, git, and more #####"
+yum install yum-utils nfs-utils bash-completion git -y
+
+echo "##### Disable firewall starting from Kubernetes v1.19 onwards #####"
+systemctl disable firewalld --now
+
+
+echo "##### Letting ipTables see bridged networks #####"
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+##
+## iptables config as specified by CRI-O documentation
+# Create the .conf file to load the modules at bootup
+cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
+overlay
+br_netfilter
+EOF
+
+
+modprobe overlay
+modprobe br_netfilter
+
+
+echo "##### Set up required sysctl params, these persist across reboots #####"
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+
+
+###
+echo "##### Configuring Kubernetes repositories #####"
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=0
+EOF
+
+echo "##### Set SELinux in permissive mode (effectively disabling it) #####"
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+echo "##### Disable swap #####"
+swapoff -a
+
+echo "##### make a backup of fstab #####"
+cp -f /etc/fstab /etc/fstab.bak
+
+echo "##### Remove swap from fstab #####"
+sed -i '/swap/d' /etc/fstab
+
+
+echo "##### Refresh repo list #####"
+yum repolist -y
+
+echo "##### Configuring CRIO #####"
+
+#set OS version
+OS=CentOS_7
+
+#set CRI-O
+VERSION=1.24
+
+# Install CRI-O
+sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/devel:kubic:libcontainers:stable.repo
+sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo
+sudo yum install cri-o -y
+
+
+##Install Kubernetes, specify Version as CRI-O
+yum install -y kubelet kubeadm kubectl
+
+kubeadm init
+systemctl enable --now crio
+systemctl enable --now kubelet
+
